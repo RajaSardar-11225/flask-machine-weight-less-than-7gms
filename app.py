2ee6fa962cfe28c18eb110110a898ecae6c93bcf
RajaSardar-11225 <dme1@safed.in>
2026-05-14 13:45:00 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import pymssql
+
+server = 'sdplserver.database.windows.net'
+database = 'dme'
+user_name = 'dmeadmin'
+password = 'Agri@774Safe#14'
+
+
+#######################################################################################
+
+from flask import Flask, render_template, request
+
+app = Flask("__name__")
+
+@app.route("/", methods=["GET", "POST"])
+
+def home():
+
+    now_connect = pymssql.connect(server= server, user= user_name, password= password, database= database)
+    cursor = now_connect.cursor()
+    query = """select top 7 * from Daily_Packing_Data where Location ='Agarpara' and Weight_of_Bag<7 order by Weight_of_Bag DESC;"""
+    cursor.execute(query)
+    result = cursor.fetchall()
+
+
+    query2 = """select top 7 * from Daily_Packing_Data where Location ='Agarpara' and Weight_of_Bag>7.5 order by Weight_of_Bag ASC;"""
+    cursor.execute(query2)
+    result2 = cursor.fetchall()
+    now_connect.close()
+    return render_template("index.html",ans=result, ans2=result2)
+
+if __name__=="__main__":
+    app.run(debug=True)
